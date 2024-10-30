628f07e4e9add34c38e57019a2e7317cc3c68c2d
Julian Engel <julian@codeero.com>
2024-10-30 09:11:41 +0200

    sitemap, animation fixes, typescript error removal,

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
 
 export default defineConfig({
+    site: 'https://meome.com', // Replace with your actual domain
     integrations: [
         tailwind({
             // Ensure Tailwind classes are included
